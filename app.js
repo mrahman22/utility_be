c59563d1ec354e22a15c8624f810b9022de0555b
mustabur <mustaburrahman2017@hotmail.com>
2020-11-21 10:48:02 +0000

    I have created mongoDB database and have established connection

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const apiRouter = require('./routers/apiRouter');
-
+const db_connection = require('./db_connection');
+const apiRouter = require("./routers/apiRouter");
 
 // express app
 const app = express();
 // Recognizes incoming request object as a JSON object
 app.use(express.json());
 
+// Connect to mondoDB
+// const dbURI =
+//   "mongodb+srv://mrahman22:Determination@23@products.gagnl.mongodb.net/<dbname>?retryWrites=true&w=majority";
+mongoose
+  .connect(db_connection, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("Connected to DB"))
+  .catch((err) => {
+    console.log(err);
+  });
 
-// Listening for incoming requests
-app.listen(3000, () => {
-    console.log("Listening on port 3000......")
-})
 
+app.use("/api", apiRouter);
 
-app.use("/api", apiRouter)
-
-
-
-module.exports = app
+module.exports = app;
