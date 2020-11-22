05cb8ffa6fffbdbdbf49901fb31b9f682d8508cd
mustabur <mustaburrahman2017@hotmail.com>
2020-11-22 12:36:24 +0000

    reinstalled npm packages

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+const db_connection = require('./db_connection');
 const apiRouter = require('./routers/apiRouter');
 
 
 // express app
 const app = express();
-app.use(cors());
 
 //Connect to mondoDB using mongoose
-
-const db_connection = "mongodb+srv://mrahman22:Determination@23@products.gagnl.mongodb.net/utility?retryWrites=true&w=majority";
-
 mongoose
   .connect(db_connection, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => console.log("Connected to DB"))
@@ -19,19 +15,19 @@ mongoose
     console.log(err);
   });
 
+
 // Recognizes incoming request object as a JSON object
 app.use(express.json());
 
-app.use("/api", apiRouter)
 
-app.use("/", (req, res) => {
-  res.send("Route Does Not Exist")
+// Listening for incoming requests
+app.listen(3000, () => {
+    console.log("Listening on port 3000......")
 })
 
 
-app.use((err, req, res, next) => {
-  res.status(err.status).send({ msg: err.msg });
-});
+app.use("/api", apiRouter)
+
 
 
 module.exports = app
