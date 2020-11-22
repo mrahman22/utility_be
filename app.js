c677eb8a874643b90e4072b9cd92ad402a6f8a7a
mustabur <mustaburrahman2017@hotmail.com>
2020-11-22 12:22:55 +0000

    db connection

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const db_connection = require('./db_connection');
 const apiRouter = require('./routers/apiRouter');
 
 
