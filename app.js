a26ecfa7100e9efe9024761b6091dd42f52f5e31
mustabur <mustaburrahman2017@hotmail.com>
2020-11-22 12:12:24 +0000

    db connecton

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(cors());
 
 //Connect to mondoDB using mongoose
+
+const db_connection = "mongodb+srv://mrahman22:Determination@23@products.gagnl.mongodb.net/utility?retryWrites=true&w=majority";
+
 mongoose
   .connect(db_connection, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => console.log("Connected to DB"))
