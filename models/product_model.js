65b75eeac2fca35466662a201bac76adfd061413
mustabur <mustaburrahman2017@hotmail.com>
2020-11-22 10:20:08 +0000

    change on made on requesting products

--- models/product_model.js
+++ models/product_model.js
@@ -3,12 +3,13 @@ const Product = require("../db/schema");
 const fetchProducts = ({ sortby, price }) => {
 
 
-  return Product.find({'_id': false})
+  return Product.find()
     .sort([
       [sortby, "asc"],
       ["price", price],
     ])
     .then((products) => {
+      console.log(products);
       if (!products) {
         return Promise.reject({ status: 404, msg: "Sorry no products were found found" });
       }
